2c39f2bcfaccb90320f3ad404ba0505e6e323b34
redtardis12 <egorkaklim98@gmail.com>
2021-02-10 23:14:51 +0300

    demo scene

---
+++ DemoScene/StylizedHandPaintedDungeon(Free)/Demo/Demo_Scene/ExibitThird.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExibitThird : MonoBehaviour
+{
+    public Transform[] target;
+    public float speed;
+
+    private int current;
+   
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != target[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else
+        {
+            current = (current + 1) % target.Length;
+        }
+    }
+}
